471e678cd0df709e855e6e3360ca6421cd5ea0ed
Austin Conlon <austinconlon@icloud.com>
2020-04-04 17:09:32 -0700

    Initial Commit

---
+++ Hacker News WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hacker News WatchKit Extension
+//
+//  Created by Austin Conlon on 4/4/20.
+//  Copyright © 2020 Austin Conlon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hacker News WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  Hacker News WatchKit Extension
+//
+//  Created by Austin Conlon on 4/4/20.
+//  Copyright © 2020 Austin Conlon. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
